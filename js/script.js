4c3e4c5b508cb60bf71b2a04e30ad9853bc6c534
brianbouquet <59177725+brianbouquet@users.noreply.github.com>
2019-12-30 11:58:05 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -11,19 +11,69 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+let quotes = [
+  {
+    quote: "Reality is wrong. Dreams are for real.",
+    source: "Tupac Shakur",
+    citation: "So Many Tears",
+    year: 1995
+  },
+  {
+    quote: "Being happy is my goal, but greatness is my mission.",
+    source: "Childish Gambino",
+    citation: "I'm Alright",
+    year: 2010
+  },
+  {
+    quote: "Nobody built like you, you design yourself.",
+    source: "Jay-Z",
+    citation: "A Dream",
+    year: 2002
+  },
+  {
+    quote: "Everybody dies, but not everybody lives.",
+    source: "Drake",
+    citation: "Moment 4 Life",
+    year: 2010
+  },
+  {
+    quote: "Without order, nothing exists. Without chaos, nothing evolves.",
+    source: "Jedi Mind Tricks",
+    citation: "Heavy Metal Kings",
+    year: 2006
+  },
+  {
+    quote: "We can't change the world unless we change ourselves.",
+    source: "The Notorious B.I.G.",
+    citation: "When I Die...",
+    year: 1994
+  }
+];
 
+//log quotes object to console
+console.log(quotes);
 
 /***
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote(quotes) {
+  let randomNumber = Math.floor(Math.random() * quotes.length);
+  return quotes[randomNumber];
+}
 
+//log randomly selected quote to console
+console.log(getRandomQuote(quotes));
 
 /***
  * `printQuote` function
 ***/
 
-
+function printQuote(quotes) {
+  let quoteSelected = getRandomQuote(quotes);
+  let html = '';
+  html += ''
+}
 
 /***
  * click event listener for the print quote button
